42457edc5c8ee6c6b58185faa7604f52ed621385
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-02-20 20:22:58 +0700

    re init github

--- components/Overview.vue
+++ components/Overview.vue
@@ -3,18 +3,18 @@ import { VisAxis, VisStackedBar, VisXYContainer } from '@unovis/vue'
 
 type Data = typeof data[number]
 const data = [
-  { name: 'Jan', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Feb', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Mar', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Apr', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'May', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Jun', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Jul', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Aug', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Sep', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Oct', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Nov', total: Math.floor(Math.random() * 5000) + 1000 },
-  { name: 'Dec', total: Math.floor(Math.random() * 5000) + 1000 },
+  { name: 'Jan', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Feb', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Mar', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Apr', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'May', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Jun', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Jul', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Aug', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Sep', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Oct', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Nov', total: Math.floor(Math.random() * 10) + 20 },
+  { name: 'Dec', total: Math.floor(Math.random() * 10) + 20 },
 ]
 </script>
 
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -55,9 +55,6 @@ const tab = ref()
                 <div class="text-2xl font-bold">
                   10.000 item
                 </div>
-                <p class="text-xs text-muted-foreground">
-                  +1.1% dari bulan kemarin
-                </p>
               </CardContent>
             </Card>
             <Card>
@@ -65,20 +62,7 @@ const tab = ref()
                 <CardTitle class="text-sm font-medium">
                   Total Ruangan
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  class="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
+                <Icon name="arcticons:utd-rooms" />
               </CardHeader>
               <CardContent>
                 <div class="text-2xl font-bold">
@@ -91,19 +75,7 @@ const tab = ref()
                 <CardTitle class="text-sm font-medium">
                   Total Gedung
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  class="h-4 w-4 text-muted-foreground"
-                >
-                  <rect width="20" height="14" x="2" y="5" rx="2" />
-                  <path d="M2 10h20" />
-                </svg>
+                <Icon name="ph:buildings"/>
               </CardHeader>
               <CardContent>
                 <div class="text-2xl font-bold">
@@ -116,33 +88,19 @@ const tab = ref()
                 <CardTitle class="text-sm font-medium">
                   User Aktif
                 </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  class="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                </svg>
+                <Icon name="ph:users" />
               </CardHeader>
               <CardContent>
                 <div class="text-2xl font-bold">
                   30
                 </div>
-                <p class="text-xs text-muted-foreground">
-                  +2 dari bulan lalu
-                </p>
               </CardContent>
             </Card>
           </div>
           <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
             <Card class="col-span-4">
               <CardHeader>
-                <CardTitle>Overview</CardTitle>
+                <CardTitle>Statistik Transaksi</CardTitle>
               </CardHeader>
               <CardContent class="pl-2">
                 <Overview />
