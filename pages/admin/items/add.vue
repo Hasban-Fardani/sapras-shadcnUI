2ee395cae8b469a6b2079bd6629db61699308ad7
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-02-23 15:23:04 +0700

    feat: add form and fix error build

--- pages/admin/items/add.vue
+++ pages/admin/items/add.vue
@@ -1,8 +1,40 @@
-<script setup> 
+<script setup>
 definePageMeta({
-  layout: 'admin'
+    layout: 'admin'
 })
+
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50),
+}))
+
+const form = useForm({
+  validationSchema: formSchema,
+})
+
+const onSubmit = () => {
+    console.log('horee');
+}
 </script>
 <template>
-    
+    <form @submit="onSubmit">
+    <FormField v-slot="{ componentField }" name="username">
+      <FormItem>
+        <FormLabel>Username</FormLabel>
+        <FormControl>
+          <Input type="text" placeholder="shadcn" v-bind="componentField" />
+        </FormControl>
+        <FormDescription>
+          This is your public display name.
+        </FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+    <Button type="submit">
+      Submit
+    </Button>
+  </form>
 </template>
--- pages/admin/transactions/[id].vue
+++ pages/admin/transactions/[id].vue
@@ -1,13 +1,9 @@
 <script setup>
-import { items } from "./data";
 
 definePageMeta({
     layout: 'admin'
 })
 
-const id = useRoute().params.id;
-let item = items.find(i => i.id == id);
-console.log(item);
 </script>
 <template>
 
